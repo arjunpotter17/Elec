208ae72e11eb3dffc3ca8aa14b5030aeb1836577
arjunpotter17 <arjunpotter17@gmail.com>
2023-02-25 15:16:58 +0530

    home button added

---
+++ components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link';
+import { client } from '@/lib/client';
+import Image from 'next/image';
+
+
+const Home = ({logo}) => {
+  console.log('this is logo',logo)
+  return (
+    <Link href='https://portfolio-six-gold-18.vercel.app/'>
+    <div className='home-btn'>
+        <Image src='/assets/footer-logo.jpg' alt='portfolio' width={55} height={55} style={{borderRadius:'50%', border:'none', objectFit:'cover', margin:'auto'}}/>
+    </div>
+    </Link>
+    
+  )
+}
+
+export const getServerSideProps = async () => {
+    const query = '*[_type == "logo"]';
+    const logo = await client.fetch(query);
+    
+  
+    return {
+      props: { logo }
+    }
+  }
+
+export default Home
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-
+import Home from './Home';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
@@ -18,7 +18,9 @@ const Layout = ({ children,user,setUser,key,setKey }) => {
       </main>
       <footer>
         <Footer />
+        <Home/>
       </footer>
+      
     </div>
   )
 }
